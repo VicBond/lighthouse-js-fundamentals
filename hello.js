55c5a807b5f7420c9fe6b9add3a32a4884af1b3f
Victor Bondarenko <nirasimha@gmail.com>
2021-09-26 13:27:30 +0000

    Return vs console.log

--- hello.js
+++ hello.js
@@ -1,7 +1,17 @@
-const sayHello = function (name){
+/*const sayHello = function (name){
   console.log("Hello, " + name);
 }
 
 sayHello("Dany");
 sayHello("Mana");
 sayHello("John");
+*/
+
+//Return vs console.log
+
+const returnSayHello = function (name){
+  return "Hi, " + name;
+
+}
+const greeting = returnSayHello ("Maya");
+console.log(greeting);
